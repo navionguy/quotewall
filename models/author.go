ecb0f98761d6d857311dc8ab4d5e6aae3b8d416c
Don Hinds <hindsdw@gmail.com>
2020-10-09 16:26:56 -0400

    added the author model

---
+++ models/author.go
@@ -0,0 +1,136 @@
+package models
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+)
+
+// Author holds the name of somebody who authored a quote
+type Author struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Name      string    `json:"name" db:"name" form:"name"`
+}
+
+// AuthorCredit allows me to find out how many quotes each author has
+type AuthorCredit struct {
+	ID    uuid.UUID `json:"id" db:"id"`
+	Name  string    `json:"name" db:"name"`
+	Count int       `json:"count" db:"count"`
+}
+
+// AuthorCredits holds all the authors
+type AuthorCredits []AuthorCredit
+
+// String is not required by pop and may be deleted
+func (a Author) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Authors is not required by pop and may be deleted
+type Authors []Author
+
+// String is not required by pop and may be deleted
+func (a Authors) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (a *Author) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+	//&validators.StringIsPresent{Field: a.Name, Name: "Name"},
+	//&validators.StringLengthInRange{Field: a.Name, Name: "Name", Min: 1, Max: 255, Message: "length must be 1-255"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (a *Author) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (a *Author) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// SelectValue returns the author ID value to a form SelectTag
+func (a Author) SelectValue() interface{} {
+	return a.ID.String()
+}
+
+// SelectLabel allows authors to be in a form SelectTag
+func (a Author) SelectLabel() string {
+	return a.Name
+}
+
+// FindByID pulls up the author record based on ID
+func (a *Author) FindByID() error {
+
+	authRecs := []Author{}
+	query := DB.Where(fmt.Sprintf("id = '%s'", a.ID))
+	err := query.All(&authRecs)
+
+	if err != nil {
+		return err
+	}
+
+	if len(authRecs) == 0 {
+		return errors.New("author ID not found in db")
+	}
+
+	*a = authRecs[0]
+
+	return nil
+}
+
+// FindByName check for an author by name
+func (a *Author) FindByName() error {
+	// name can't be empty
+	if len(a.Name) == 0 {
+		return errors.New("author name can't be blank")
+	}
+
+	// Break the passed name down into pieces
+	parts := strings.Split(a.Name, " ")
+
+	authRecs := []Author{}
+	query := DB.Where(fmt.Sprintf("name ILIKE '%%%s%%' AND name ILIKE '%%%s%%'", parts[0], parts[len(parts)-1]))
+	err := query.All(&authRecs)
+
+	if err != nil {
+		return err
+	}
+
+	if len(authRecs) == 0 {
+		return errors.New("author name not found in db")
+	}
+
+	*a = authRecs[0]
+
+	return nil
+}
+
+// Create adds a new speaker to the authors table
+func (a *Author) Create() (*validate.Errors, error) {
+	verr, err := DB.ValidateAndCreate(a)
+
+	return verr, err
+}
+
+// Update modifies an already saved author
+func (a *Author) Update() (*validate.Errors, error) {
+	return nil, nil
+}
---
+++ models/author_test.go
@@ -0,0 +1,171 @@
+package models_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gobuffalo/suite"
+	"github.com/gobuffalo/uuid"
+	"github.com/navionguy/quotewall/models"
+	"github.com/stretchr/testify/require"
+)
+
+type ModelSuite struct {
+	*suite.Model
+}
+
+func Test_Author(t *testing.T) {
+
+	var fields = []struct {
+		fn  string
+		msg string
+	}{
+		{"id", "author id field not found"},
+		{"name", "author name field not found"},
+		{"created_at", "created_at field not found"},
+		{"updated_at", "updated_at field not found"},
+	}
+
+	a := models.Author{
+		Name: "George B. Burdell",
+	}
+
+	// convert author to json
+
+	js := a.String()
+
+	// if you get nothing, that's a problem
+
+	if len(js) == 0 {
+		t.Error("unable to marshal author")
+		t.FailNow()
+	}
+
+	// make sure expected fields are there
+
+	rq := require.New(t)
+
+	for _, fld := range fields {
+		rq.Containsf(js, fld.fn, fld.msg)
+	}
+
+	var ar models.Authors
+
+	ar = append(ar, a)
+
+	js = ar.String()
+
+	if len(js) == 0 {
+		t.Error("Unable to marshal array of authors")
+		t.Fail()
+	}
+}
+
+const invalidUUID = "563cd207-ab16-4a46-b44e-7317b96c6ba9"
+const validUUID = "b39300f0-6760-4feb-bc32-4b8682b0175d" // matches entry in testauthors.toml
+const validName = "George P. Burdell"
+
+// Test for finding an existing author
+func (ms *ModelSuite) Test_Author_FindByID() {
+	//	ms.LoadFixture("test authors")
+
+	id, err := uuid.FromString(validUUID)
+
+	if err != nil {
+		ms.Fail("uuid.FromString failed", err.Error())
+	}
+
+	auth := models.Author{
+		ID: id,
+	}
+
+	err = auth.FindByID()
+
+	if err != nil {
+		ms.Fail("FindByID failed", err.Error())
+	}
+
+	if len(auth.Name) == 0 {
+		ms.Fail("FindByID failed", "validUUID was not found in database")
+	}
+
+	if strings.Compare(auth.Name, validName) != 0 {
+		ms.Fail("FindByID didn't find expected author", auth.Name)
+	}
+
+	// as long as I have a valid author, check some other functions
+
+	if strings.Compare(auth.SelectLabel(), validName) != 0 {
+		ms.Fail("unexpected SelectLabel", auth.SelectLabel())
+	}
+
+	v := auth.SelectValue()
+	s, ok := v.(string)
+
+	if ok {
+		if strings.Compare(s, validUUID) != 0 {
+			ms.Fail("unexpected SelectValue", s)
+		}
+	} else {
+		ms.Fail("unexpected SelectValue", "not a string")
+	}
+}
+
+// test that FindByID correctly handles NOT finding the author
+func (ms *ModelSuite) Test_Author_FindByID_BadID() {
+	ms.LoadFixture("test authors")
+
+	id, err := uuid.FromString(invalidUUID)
+
+	if err != nil {
+		ms.Fail("uuid.FromString failed", err.Error())
+	}
+
+	auth := models.Author{
+		ID: id,
+	}
+
+	err = auth.FindByID()
+
+	if err != nil {
+		ms.Fail("FindByID failed", err.Error())
+	}
+
+	if len(auth.Name) != 0 {
+		ms.Fail("FindByID succeeded with an invalid UUID", auth.Name)
+	}
+}
+
+func (ms *ModelSuite) Test_Author_Create() {
+	ms.LoadFixture("test authors")
+
+	auth := models.Author{
+		Name: "Brand New Author",
+	}
+
+	verrs, err := ms.DB.ValidateAndCreate(&auth)
+
+	if err != nil {
+		ms.Fail("failed to create author", err.Error())
+	}
+
+	if verrs.HasAny() {
+		ms.Fail("unable to validate new author", verrs.String())
+	}
+}
+
+func (ms *ModelSuite) Test_Author_CreateInvalid() {
+	ms.LoadFixture("test authors")
+
+	auth := models.Author{}
+
+	verrs, err := ms.DB.ValidateAndCreate(&auth)
+
+	if err != nil {
+		ms.Fail("failed to create author", err.Error())
+	}
+
+	if !verrs.HasAny() {
+		ms.Fail("invalid author validated", "no name supplied")
+	}
+}
